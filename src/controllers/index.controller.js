dfdeaacfbc9fc097a37874d5d6c665640b612ae8
hari6091 <hari.junior19@gmail.com>
2023-10-25 11:37:56 -0300

    feat: update passwd

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,7 +4,7 @@ const createDatabase = async () => {
   const poolWithoutDatabase = new Pool({
     user: "postgres",
     host: "database",
-    password: "pgpassword",
+    password: "",
     database: "postgres",
     port: "5432",
   });
